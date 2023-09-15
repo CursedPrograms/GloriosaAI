412bc362dab0a8e47f413f09b7f189db46c07ae8
Cursed Entertainment <110344485+CursedPrograms@users.noreply.github.com>
2023-09-15 10:17:40 +0200

    Update TensorSnake0-0-1.py

--- TensorSnake0-0-1.py
+++ TensorSnake0-0-1.py
@@ -6,7 +6,6 @@ from tensorflow.keras.layers import Dense, Reshape, Flatten, Conv2D, Conv2DTrans
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-# Define the generator model
 def build_generator(input_dim):
     generator = Sequential()
     generator.add(Dense(7 * 7 * 256, input_dim=input_dim))
@@ -16,7 +15,6 @@ def build_generator(input_dim):
     generator.add(Conv2D(3, (7, 7), padding='same', activation='sigmoid'))
     return generator
 
-# Define the discriminator model
 def build_discriminator(input_shape):
     discriminator = Sequential()
     discriminator.add(Conv2D(64, (3, 3), strides=(2, 2), padding='same', input_shape=input_shape, activation='relu'))
@@ -25,18 +23,15 @@ def build_discriminator(input_shape):
     discriminator.add(Dense(1, activation='sigmoid'))
     return discriminator
 
-# Hyperparameters
 epochs = 10000
 batch_size = 64
 input_dim = 100
 output_dir = "output_images"
 model_save_dir = "saved_models"
-image_shape = (28, 28, 3)  # RGB images with size 28x28
-
+image_shape = (28, 28, 3)       
 os.makedirs(output_dir, exist_ok=True)
 os.makedirs(model_save_dir, exist_ok=True)
 
-# Define a function to load and preprocess your dataset
 def load_and_preprocess_dataset(target_size):
     datagen = ImageDataGenerator(
         rescale=1. / 255,
@@ -45,33 +40,26 @@ def load_and_preprocess_dataset(target_size):
     dataset_dir = os.path.join(script_dir, "training_data")
 
     dataset = datagen.flow_from_directory(
-        dataset_dir,  # Use the training_data folder relative to the script
+        dataset_dir,          
         target_size=target_size,
         batch_size=batch_size,
-        class_mode='input'  # Use 'input' mode for autoencoders
+        class_mode='input'       
     )
     return dataset
 
-# Load and preprocess the dataset
-dataset = load_and_preprocess_dataset(image_shape[:2])  # Adjust target_size
-
-# Build the generator and discriminator models
+dataset = load_and_preprocess_dataset(image_shape[:2])    
 generator = build_generator(input_dim)
 discriminator = build_discriminator(image_shape)
 
-# Compile the discriminator model
 discriminator.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(0.0002, 0.5), metrics=['accuracy'])
 
-# Make the discriminator non-trainable during GAN training
 discriminator.trainable = False
 
-# Build the GAN model
 gan_input = Input(shape=(input_dim,))
 x = generator(gan_input)
 gan_output = discriminator(x)
 gan = Model(gan_input, gan_output)
 
-# Compile the GAN model
 gan.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(0.0002, 0.5))
 
 def generate_and_save_images(generator, epoch, output_dir, num_examples=16):
@@ -85,12 +73,10 @@ def generate_and_save_images(generator, epoch, output_dir, num_examples=16):
         plt.imshow(generated_images[i])
         plt.axis('off')
 
-    plt.savefig(os.path.join(output_dir, f"generated_image_epoch_{epoch}.png"))  # Save the figure
-
+    plt.savefig(os.path.join(output_dir, f"generated_image_epoch_{epoch}.png"))     
     plt.show()
     plt.close(fig)
 
-# Training loop
 for epoch in range(epochs):
     if epoch % 1000 == 0:
         generator_model_save_path = os.path.join(model_save_dir, f"gan_generator_weights_epoch_{epoch}.h5")
@@ -102,14 +88,12 @@ for epoch in range(epochs):
 
     for _ in range(0, len(dataset), batch_size):
         real_batch = next(dataset)
-        real_images = real_batch[0]  # Extract real images from the batch
-        real_labels = np.ones((real_images.shape[0], 1))  # Match the batch size
-
+        real_images = real_batch[0]        
+        real_labels = np.ones((real_images.shape[0], 1))      
         noise = np.random.normal(0, 1, (real_images.shape[0], input_dim))
 
         generated_images = generator.predict(noise)
-        fake_labels = np.zeros((real_images.shape[0], 1))  # Match the batch size
-
+        fake_labels = np.zeros((real_images.shape[0], 1))      
         d_loss_real = discriminator.train_on_batch(real_images, real_labels)
         d_loss_fake = discriminator.train_on_batch(generated_images, fake_labels)
         d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
