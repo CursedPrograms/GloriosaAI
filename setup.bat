76fe4628086d92dd1a55531695dde2c27932dedd
Cursed Entertainment <110344485+CursedPrograms@users.noreply.github.com>
2024-01-25 03:22:18 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,15 +1,3 @@
 @echo off
-
-set "VENV_DIR=psdenv"
-
-rem 
-if not exist "%VENV_DIR%" (
-    rem 
-    python -m venv "%VENV_DIR%"
-)
-
-rem 
-call "%VENV_DIR%\Scripts\activate" && python main.py
-
-rem 
-pause
+python -m venv psdenv
+cmd /k ".\psdenv\Scripts\activate & python main.py"
